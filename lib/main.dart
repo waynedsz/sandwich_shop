e659efd0b5ef8456ba12fbe2c25946a59bfc74b2
manighahrmani <mashapicasso@gmail.com>
2025-05-17 05:19:57 +0100

    📝 Update main.dart to include 'super.key' in SandwichCounter and SandwichShopApp constructors for improved widget key handling

--- lib/main.dart
+++ lib/main.dart
@@ -748,7 +748,7 @@ class SandwichCounter extends StatelessWidget {
   final String sandwichType;
   final int count;
 
-  const SandwichCounter(this.count, this.sandwichType);
+  const SandwichCounter(this.count, this.sandwichType, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -757,10 +757,12 @@ class SandwichCounter extends StatelessWidget {
 }
 
 void main() {
-  runApp(SandwichShopApp());
+  runApp(const SandwichShopApp());
 }
 
 class SandwichShopApp extends StatelessWidget {
+  const SandwichShopApp({super.key});
+
   @override
   Widget build(BuildContext context) {
 <<<<<<< HEAD
