98f177970682971ea6659010be6d8e90d66ee8ce
manighahrmani <mashapicasso@gmail.com>
2025-08-27 02:08:44 +0100

    Add a key to the notes text field

--- lib/main.dart
+++ lib/main.dart
@@ -605,6 +605,9 @@ class _OrderScreenState extends State<OrderScreen> {
             Padding(
               padding: const EdgeInsets.all(40.0),
               child: TextField(
+                // We need a key to distinguish this TextField from the
+                // TextFields that are used in the DropdownMenu (for testing).
+                key: const Key('notes_textfield'),
                 controller: _notesController,
                 decoration: const InputDecoration(
                   labelText: 'Add a note (e.g., no onions)',
