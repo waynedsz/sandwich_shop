4ac319d72f295311fca0597bdb3f3895d8b7ff04
manighahrmani <mashapicasso@gmail.com>
2025-09-07 14:29:24 +0100

    Add cart unit tests

--- test/models/cart_test.dart
+++ test/models/cart_test.dart
@@ -4,6 +4,7 @@ import 'package:sandwich_shop/models/sandwich.dart';
 
 void main() {
   group('Cart', () {
+<<<<<<< HEAD
     final sandwich1 = Sandwich(
       type: SandwichType.veggieDelight,
       isFootlong: false,
@@ -72,6 +73,95 @@ void main() {
       cart.addItem(sandwich1);
       expect(cart.contains(sandwich1), isTrue);
       expect(cart.contains(sandwich2), isFalse);
+=======
+    late Cart cart;
+    late Sandwich sandwichA;
+    late Sandwich sandwichB;
+
+    setUp(() {
+      cart = Cart();
+      sandwichA = Sandwich(
+          name: 'A',
+          isFootlong: false,
+          breadType: BreadType.wheat,
+          image: 'assets/images/a.png');
+      sandwichB = Sandwich(
+          name: 'B',
+          isFootlong: true,
+          breadType: BreadType.white,
+          image: 'assets/images/b.png');
+    });
+
+    test('should start empty', () {
+      expect(cart.isEmpty, isTrue);
+      expect(cart.length, 0);
+      expect(cart.items, isEmpty);
+    });
+
+    test('should add a sandwich with default quantity 1', () {
+      cart.add(sandwichA);
+      expect(cart.getQuantity(sandwichA), 1);
+      expect(cart.length, 1);
+      expect(cart.isEmpty, isFalse);
+    });
+
+    test('should add a sandwich with custom quantity', () {
+      cart.add(sandwichA, quantity: 3);
+      expect(cart.getQuantity(sandwichA), 3);
+      expect(cart.length, 1);
+    });
+
+    test('should increase quantity if same sandwich is added again', () {
+      cart.add(sandwichA);
+      cart.add(sandwichA, quantity: 2);
+      expect(cart.getQuantity(sandwichA), 3);
+    });
+
+    test('should add multiple different sandwiches', () {
+      cart.add(sandwichA);
+      cart.add(sandwichB, quantity: 2);
+      expect(cart.getQuantity(sandwichA), 1);
+      expect(cart.getQuantity(sandwichB), 2);
+      expect(cart.length, 2);
+    });
+
+    test('should remove quantity of a sandwich', () {
+      cart.add(sandwichA, quantity: 3);
+      cart.remove(sandwichA, quantity: 2);
+      expect(cart.getQuantity(sandwichA), 1);
+    });
+
+    test('should remove sandwich completely if quantity drops to zero', () {
+      cart.add(sandwichA, quantity: 2);
+      cart.remove(sandwichA, quantity: 2);
+      expect(cart.getQuantity(sandwichA), 0);
+      expect(cart.isEmpty, isTrue);
+      expect(cart.length, 0);
+    });
+
+    test('should not throw an error when removing a sandwich not in cart', () {
+      expect(() => cart.remove(sandwichA), returnsNormally);
+    });
+
+    test('should clear all items', () {
+      cart.add(sandwichA);
+      cart.add(sandwichB);
+      cart.clear();
+      expect(cart.isEmpty, isTrue);
+      expect(cart.items, isEmpty);
+    });
+
+    test('getQuantity returns correct quantity', () {
+      cart.add(sandwichA, quantity: 4);
+      expect(cart.getQuantity(sandwichA), 4);
+      expect(cart.getQuantity(sandwichB), 0);
+    });
+
+    test('totalPrice calculates sum using PricingRepository', () {
+      cart.add(sandwichA, quantity: 2); // 2 * 7 = 14
+      cart.add(sandwichB, quantity: 1); // 1 * 11 = 11
+      expect(cart.totalPrice, 25.0);
+>>>>>>> 189705e (Add cart unit tests)
     });
   });
 }
