dffb194784150115611524bc52903efc013e29c5
manighahrmani <mashapicasso@gmail.com>
2025-09-07 03:04:29 +0100

    Add unit test for sandwich

--- test/models/sandwich_test.dart
+++ test/models/sandwich_test.dart
@@ -4,6 +4,7 @@ import 'package:sandwich_shop/models/sandwich.dart';
 void main() {
   group('Sandwich', () {
 <<<<<<< HEAD
+<<<<<<< HEAD
     test('name returns correct string', () {
       expect(
         Sandwich(
@@ -61,11 +62,35 @@ void main() {
           type: SandwichType.tunaMelt,
           isFootlong: false,
           breadType: bread,
+=======
+    test('should create a Sandwich with correct properties', () {
+      final sandwich = Sandwich(
+        name: 'Veggie Delight',
+        isFootlong: true,
+        breadType: BreadType.wholemeal,
+        image: 'assets/images/veggie.png',
+      );
+
+      expect(sandwich.name, 'Veggie Delight');
+      expect(sandwich.isFootlong, isTrue);
+      expect(sandwich.breadType, BreadType.wholemeal);
+      expect(sandwich.image, 'assets/images/veggie.png');
+    });
+
+    test('should support all BreadType enum values', () {
+      for (final bread in BreadType.values) {
+        final sandwich = Sandwich(
+          name: 'Test',
+          isFootlong: false,
+          breadType: bread,
+          image: 'assets/images/test.png',
+>>>>>>> d01b10f (Add unit test for sandwich)
         );
         expect(sandwich.breadType, bread);
       }
     });
 
+<<<<<<< HEAD
     test('should support all SandwichType enum values and correct names', () {
       final Map<SandwichType, String> expectedNames = {
         SandwichType.veggieDelight: 'Veggie Delight',
@@ -107,3 +132,70 @@ void main() {
 >>>>>>> d01b10f (Add unit test for sandwich)
 =======
 >>>>>>> 2471fd2 (Update the sandwich test accordingly)
+=======
+    test('should handle isFootlong true and false', () {
+      final footlong = Sandwich(
+        name: 'Footlong',
+        isFootlong: true,
+        breadType: BreadType.white,
+        image: 'assets/images/footlong.png',
+      );
+      final sixInch = Sandwich(
+        name: 'Six Inch',
+        isFootlong: false,
+        breadType: BreadType.white,
+        image: 'assets/images/six-inch.png',
+      );
+
+      expect(footlong.isFootlong, isTrue);
+      expect(sixInch.isFootlong, isFalse);
+    });
+
+    test('should throw if name is empty', () {
+      expect(
+        () => Sandwich(
+          name: '',
+          isFootlong: false,
+          breadType: BreadType.wheat,
+          image: 'assets/images/empty.png',
+        ),
+        throwsA(isA<ArgumentError>().having((e) => e.message, 'message', contains('Name cannot be empty'))),
+      );
+    });
+
+    test('should throw if image is empty', () {
+      expect(
+        () => Sandwich(
+          name: 'No Image',
+          isFootlong: false,
+          breadType: BreadType.wheat,
+          image: '',
+        ),
+        throwsA(isA<ArgumentError>().having((e) => e.message, 'message', contains('Image must be a valid asset path'))),
+      );
+    });
+
+    test('should throw if image does not start with assets/images/', () {
+      expect(
+        () => Sandwich(
+          name: 'Bad Image',
+          isFootlong: false,
+          breadType: BreadType.wheat,
+          image: 'bad/path.png',
+        ),
+        throwsA(isA<ArgumentError>().having((e) => e.message, 'message', contains('Image must be a valid asset path'))),
+      );
+    });
+
+    test('should allow valid asset image paths', () {
+      final sandwich = Sandwich(
+        name: 'Valid Image',
+        isFootlong: false,
+        breadType: BreadType.white,
+        image: 'assets/images/valid.png',
+      );
+      expect(sandwich.image, 'assets/images/valid.png');
+    });
+  });
+}
+>>>>>>> d01b10f (Add unit test for sandwich)
