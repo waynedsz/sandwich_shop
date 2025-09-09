f4c961502451cd0badd7995255cabf7994a908c7
manighahrmani <mashapicasso@gmail.com>
2025-09-09 02:28:33 +0100

    Add tests for the confirmation message

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -532,6 +532,7 @@ void main() {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     testWidgets('Tapping add button increases quantity',
         (WidgetTester tester) async {
       await tester.pumpWidget(const App());
@@ -616,6 +617,9 @@ void main() {
 >>>>>>> 1529004 (Remove comments from widget_test.dart)
       expect(find.byType(Image), findsOneWidget);
 >>>>>>> f1772b8 (Update widget_test)
+=======
+      expect(find.byType(Image), findsNWidgets(2));
+>>>>>>> ede51d1 (Add tests for the confirmation message)
 
       expect(find.text('Veggie Delight'), findsWidgets);
 
@@ -650,6 +654,23 @@ void main() {
   });
 
   group('OrderScreen - Interactions', () {
+    testWidgets('shows SnackBar confirmation when item is added to cart',
+        (WidgetTester tester) async {
+      const App app = App();
+      await tester.pumpWidget(app);
+
+      final Finder addToCartButtonFinder =
+          find.widgetWithText(StyledButton, 'Add to Cart');
+      await tester.ensureVisible(addToCartButtonFinder);
+      await tester.pumpAndSettle();
+      await tester.tap(addToCartButtonFinder);
+      await tester.pumpAndSettle();
+
+      const String expectedMessage =
+          'Added 1 footlong Veggie Delight sandwich(es) on white bread to cart';
+
+      expect(find.text(expectedMessage), findsOneWidget);
+    });
     testWidgets('updates sandwich type when a new option is selected',
         (WidgetTester tester) async {
       const App app = App();
@@ -703,8 +724,10 @@ void main() {
       await tester.pumpWidget(app);
 
       final Finder addButtonFinder = find.byIcon(Icons.add);
+      await tester.ensureVisible(addButtonFinder);
+      await tester.pumpAndSettle();
       await tester.tap(addButtonFinder);
-      await tester.pump();
+      await tester.pumpAndSettle();
 
       expect(find.text('2'), findsOneWidget);
     });
@@ -715,13 +738,17 @@ void main() {
       await tester.pumpWidget(app);
 
       final Finder addButtonFinder = find.byIcon(Icons.add);
+      await tester.ensureVisible(addButtonFinder);
+      await tester.pumpAndSettle();
       await tester.tap(addButtonFinder);
-      await tester.pump();
+      await tester.pumpAndSettle();
       expect(find.text('2'), findsOneWidget);
 
       final Finder removeButtonFinder = find.byIcon(Icons.remove);
+      await tester.ensureVisible(removeButtonFinder);
+      await tester.pumpAndSettle();
       await tester.tap(removeButtonFinder);
-      await tester.pump();
+      await tester.pumpAndSettle();
 
       expect(find.text('1'), findsOneWidget);
     });
@@ -733,8 +760,10 @@ void main() {
 
       final Finder removeButtonFinder =
           find.widgetWithIcon(IconButton, Icons.remove);
+      await tester.ensureVisible(removeButtonFinder);
+      await tester.pumpAndSettle();
       await tester.tap(removeButtonFinder);
-      await tester.pump();
+      await tester.pumpAndSettle();
 
       expect(find.text('0'), findsOneWidget);
       IconButton removeButton = tester.widget<IconButton>(removeButtonFinder);
@@ -746,8 +775,10 @@ void main() {
           tester.widget<StyledButton>(addToCartButtonFinder);
       expect(styledButton.onPressed, isNull);
 
+      await tester.ensureVisible(removeButtonFinder);
+      await tester.pumpAndSettle();
       await tester.tap(removeButtonFinder);
-      await tester.pump();
+      await tester.pumpAndSettle();
 
       expect(find.text('0'), findsOneWidget);
     });
