fd5e4d35f9a8f72c5d7d0a1ae6a2dd5e85b1385c
manighahrmani <mashapicasso@gmail.com>
2025-09-09 04:09:36 +0100

    Add a separate cart view

---
+++ lib/views/cart_view_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:sandwich_shop/views/app_styles.dart';
+import 'package:sandwich_shop/views/order_screen_view.dart';
+import 'package:sandwich_shop/models/cart.dart';
+import 'package:sandwich_shop/models/sandwich.dart';
+import 'package:sandwich_shop/repositories/pricing_repository.dart';
+
+class CartViewScreen extends StatefulWidget {
+  final Cart cart;
+
+  const CartViewScreen({super.key, required this.cart});
+
+  @override
+  State<CartViewScreen> createState() {
+    return _CartViewScreenState();
+  }
+}
+
+class _CartViewScreenState extends State<CartViewScreen> {
+  void _goBack() {
+    Navigator.pop(context);
+  }
+
+  String _getSizeText(bool isFootlong) {
+    if (isFootlong) {
+      return 'Footlong';
+    } else {
+      return 'Six-inch';
+    }
+  }
+
+  double _getItemPrice(Sandwich sandwich, int quantity) {
+    final PricingRepository pricingRepository = PricingRepository();
+    return pricingRepository.calculatePrice(
+      quantity: quantity,
+      isFootlong: sandwich.isFootlong,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: 100,
+            child: Image.asset('assets/images/logo.png'),
+          ),
+        ),
+        title: const Text(
+          'Cart View',
+          style: heading1,
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 20),
+              for (MapEntry<Sandwich, int> entry in widget.cart.items.entries)
+                Column(
+                  children: [
+                    Text(entry.key.name, style: heading2),
+                    Text(
+                      '${_getSizeText(entry.key.isFootlong)} on ${entry.key.breadType.name} bread',
+                      style: normalText,
+                    ),
+                    Text(
+                      'Qty: ${entry.value} - £${_getItemPrice(entry.key, entry.value).toStringAsFixed(2)}',
+                      style: normalText,
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                ),
+              Text(
+                'Total: £${widget.cart.totalPrice.toStringAsFixed(2)}',
+                style: heading2,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              StyledButton(
+                onPressed: _goBack,
+                icon: Icons.arrow_back,
+                label: 'Back to Order',
+                backgroundColor: Colors.grey,
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
