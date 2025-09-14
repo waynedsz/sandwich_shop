109f05eb4b9045ccc7b355bf48b48825640aa127
manighahrmani <mashapicasso@gmail.com>
2025-09-14 02:59:27 +0100

    Add shared preferences

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,18 @@ import FlutterMacOS
 import Foundation
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 730c01a (Add shared preferences)
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+<<<<<<< HEAD
 =======
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
 >>>>>>> 816cadc (first commit)
+=======
+>>>>>>> 730c01a (Add shared preferences)
 }
