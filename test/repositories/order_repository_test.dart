27e1784bb5666fea185c0f44c85f29fe7971abca
waynedsz <waynedesouza33@gmail.com>
2025-11-07 18:44:57 +0000

    Add order_repository_test.dart

---
+++ test/repositories/order_repository_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sandwich_shop/repositories/order_repository.dart';
+
+void main() {
+  group('OrderRepository', () {
+    test('initial quantity should be 0', () {
+      final repository = OrderRepository(maxQuantity: 5);
+      expect(repository.quantity, 0);
+    });
+
+    test('increment should increase quantity by 1', () {
+      final repository = OrderRepository(maxQuantity: 5);
+      repository.increment();
+      expect(repository.quantity, 1);
+    });
+
+    test('decrement should decrease quantity by 1', () {
+      final repository = OrderRepository(maxQuantity: 5);
+      repository.increment(); // quantity is now 1
+      repository.decrement(); // quantity is now 0
+      expect(repository.quantity, 0);
+    });
+
+    test('quantity should not exceed maxQuantity', () {
+      final repository = OrderRepository(maxQuantity: 2);
+      repository.increment(); // quantity is 1
+      repository.increment(); // quantity is 2
+      repository.increment(); // should not change
+      expect(repository.quantity, 2);
+    });
+
+    test('quantity should not go below 0', () {
+      final repository = OrderRepository(maxQuantity: 5);
+      repository.decrement(); // should not change
+      expect(repository.quantity, 0);
+    });
+  });
+}
