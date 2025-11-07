8dc4533b6cb7c140d5c5f43ee9e0c70dd9771811
waynedsz <waynedesouza33@gmail.com>
2025-11-07 18:03:37 +0000

    Update main.dart to use app_styles.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:sandwich_shop/views/app_styles.dart';
 
 void main() {
   runApp(const App());
@@ -16,91 +16,34 @@ class App extends StatelessWidget {
   }
 }
 
-class OrderItemDisplay extends StatelessWidget {
-  final String itemType;
-  final int quantity;
-
-  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      '$quantity $itemType sandwich(es): ${'🥪' * quantity}',
-      style: const TextStyle(fontSize: 16),
-    );
-  }
-}
-
 class OrderScreen extends StatefulWidget {
   final int maxQuantity;
 
   const OrderScreen({super.key, this.maxQuantity = 10});
 
   @override
-  State<OrderScreen> createState() => _OrderScreenState();
-}
-
-class StyledButton extends StatelessWidget {
-  final String label;
-  final VoidCallback onPressed;
-  final Color backgroundColor;
-  final Color textColor;
-  final bool enabled;
-
-  const StyledButton({
-    super.key,
-    required this.label,
-    required this.onPressed,
-    this.backgroundColor = Colors.red,
-    this.textColor = Colors.white,
-    this.enabled = true,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: enabled ? onPressed : null,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: backgroundColor,
-        foregroundColor: textColor,
-      ),
-      child: Text(label),
-    );
+  State<OrderScreen> createState() {
+    return _OrderScreenState();
   }
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  int _footlongQuantity = 0;
-  int _sixInchQuantity = 0;
-  String _selectedType = 'Footlong';
+  int _quantity = 0;
 
   void _increaseQuantity() {
-    setState(() {
-      if (_selectedType == 'Footlong' &&
-          _footlongQuantity < widget.maxQuantity) {
-        _footlongQuantity++;
-      } else if (_selectedType == 'Six-inch' &&
-          _sixInchQuantity < widget.maxQuantity) {
-        _sixInchQuantity++;
-      }
-    });
+    if (_quantity < widget.maxQuantity) {
+      setState(() => _quantity++);
+    }
   }
 
   void _decreaseQuantity() {
-    setState(() {
-      if (_selectedType == 'Footlong' && _footlongQuantity > 0) {
-        _footlongQuantity--;
-      } else if (_selectedType == 'Six-inch' && _sixInchQuantity > 0) {
-        _sixInchQuantity--;
-      }
-    });
+    if (_quantity > 0) {
+      setState(() => _quantity--);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final int currentQuantity =
-        _selectedType == 'Footlong' ? _footlongQuantity : _sixInchQuantity;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sandwich Counter'),
@@ -109,52 +52,38 @@ class _OrderScreenState extends State<OrderScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SegmentedButton<String>(
-              segments: const [
-                ButtonSegment(
-                  value: 'Footlong',
-                  label: Text('Footlong'),
-                ),
-                ButtonSegment(
-                  value: 'Six-inch',
-                  label: Text('Six-inch'),
-                ),
-              ],
-              selected: {_selectedType},
-              onSelectionChanged: (Set<String> newSelection) {
-                setState(() {
-                  _selectedType = newSelection.first;
-                });
-              },
+            OrderItemDisplay(
+              _quantity,
+              'Footlong',
             ),
-            const SizedBox(height: 20),
-            OrderItemDisplay(currentQuantity, _selectedType),
-            const SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                StyledButton(
-                  label: 'Add',
+                ElevatedButton(
                   onPressed: _increaseQuantity,
-                  enabled: (_selectedType == 'Footlong' &&
-                          _footlongQuantity < widget.maxQuantity) ||
-                      (_selectedType == 'Six-inch' &&
-                          _sixInchQuantity < widget.maxQuantity),
+                  child: const Text('Add'),
                 ),
-                const SizedBox(width: 16),
-                StyledButton(
-                  label: 'Remove',
+                ElevatedButton(
                   onPressed: _decreaseQuantity,
-                  enabled:
-                      (_selectedType == 'Footlong' && _footlongQuantity > 0) ||
-                          (_selectedType == 'Six-inch' && _sixInchQuantity > 0),
+                  child: const Text('Remove'),
                 ),
               ],
             ),
-            const SizedBox(height: 40),
           ],
         ),
       ),
     );
+  }
+}
+
+class OrderItemDisplay extends StatelessWidget {
+  final int quantity;
+  final String itemType;
+
+  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
   }
 }
