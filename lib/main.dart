c0eac1c092faa01d24f4fb6b999e200d68dca40f
waynedsz <waynedesouza33@gmail.com>
2025-11-07 13:35:24 +0000

    Build UI for OrderScreen

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,36 @@ class _OrderScreenState extends State<OrderScreen> {
   int _quantity = 0;
 
   @override
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandwich Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            OrderItemDisplay(
+              _quantity,
+              'Footlong',
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => print('Add button pressed!'),
+                  child: const Text('Add'),
+                ),
+                ElevatedButton(
+                  onPressed: () => print('Remove button pressed!'),
+                  child: const Text('Remove'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
