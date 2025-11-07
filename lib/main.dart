1daa1cafe576a0e63c46554ad18ec6401c975fa7
waynedsz <waynedesouza33@gmail.com>
2025-11-07 14:49:33 +0000

    Worksheet 3

--- lib/main.dart
+++ lib/main.dart
@@ -37,28 +37,70 @@ class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key, this.maxQuantity = 10});
 
   @override
-  State<OrderScreen> createState() {
-    return _OrderScreenState();
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class StyledButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final Color textColor;
+  final bool enabled;
+
+  const StyledButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+    this.backgroundColor = Colors.red,
+    this.textColor = Colors.white,
+    this.enabled = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: enabled ? onPressed : null,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        foregroundColor: textColor,
+      ),
+      child: Text(label),
+    );
   }
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  int _quantity = 0;
+  int _footlongQuantity = 0;
+  int _sixInchQuantity = 0;
+  String _selectedType = 'Footlong';
 
   void _increaseQuantity() {
-    if (_quantity < widget.maxQuantity) {
-      setState(() => _quantity++);
-    }
+    setState(() {
+      if (_selectedType == 'Footlong' &&
+          _footlongQuantity < widget.maxQuantity) {
+        _footlongQuantity++;
+      } else if (_selectedType == 'Six-inch' &&
+          _sixInchQuantity < widget.maxQuantity) {
+        _sixInchQuantity++;
+      }
+    });
   }
 
   void _decreaseQuantity() {
-    if (_quantity > 0) {
-      setState(() => _quantity--);
-    }
+    setState(() {
+      if (_selectedType == 'Footlong' && _footlongQuantity > 0) {
+        _footlongQuantity--;
+      } else if (_selectedType == 'Six-inch' && _sixInchQuantity > 0) {
+        _sixInchQuantity--;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final int currentQuantity =
+        _selectedType == 'Footlong' ? _footlongQuantity : _sixInchQuantity;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sandwich Counter'),
@@ -67,24 +109,49 @@ class _OrderScreenState extends State<OrderScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            OrderItemDisplay(
-              _quantity,
-              'Footlong',
+            SegmentedButton<String>(
+              segments: const [
+                ButtonSegment(
+                  value: 'Footlong',
+                  label: Text('Footlong'),
+                ),
+                ButtonSegment(
+                  value: 'Six-inch',
+                  label: Text('Six-inch'),
+                ),
+              ],
+              selected: {_selectedType},
+              onSelectionChanged: (Set<String> newSelection) {
+                setState(() {
+                  _selectedType = newSelection.first;
+                });
+              },
             ),
+            const SizedBox(height: 20),
+            OrderItemDisplay(currentQuantity, _selectedType),
+            const SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
+                StyledButton(
+                  label: 'Add',
                   onPressed: _increaseQuantity,
-                  child: const Text('Add'),
+                  enabled: (_selectedType == 'Footlong' &&
+                          _footlongQuantity < widget.maxQuantity) ||
+                      (_selectedType == 'Six-inch' &&
+                          _sixInchQuantity < widget.maxQuantity),
                 ),
-                const SizedBox(width: 12),
-                ElevatedButton(
+                const SizedBox(width: 16),
+                StyledButton(
+                  label: 'Remove',
                   onPressed: _decreaseQuantity,
-                  child: const Text('Remove'),
+                  enabled:
+                      (_selectedType == 'Footlong' && _footlongQuantity > 0) ||
+                          (_selectedType == 'Six-inch' && _sixInchQuantity > 0),
                 ),
               ],
             ),
+            const SizedBox(height: 40),
           ],
         ),
       ),
