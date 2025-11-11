a27660916e80dec011730ec96cbcc361941a27b2
waynedsz <waynedesouza33@gmail.com>
2025-11-11 16:13:47 +0000

    Add a new test for switch functionality

--- test/views/widget_test.dart
+++ test/views/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter/material.dart';
 import 'package:sandwich_shop/main.dart';
 
 void main() {
@@ -25,5 +25,35 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.textContaining('0 Footlong'), findsNothing);
     expect(find.textContaining('1 Footlong'), findsOneWidget);
+  });
+
+  // This test verifies the functionality of the Switch widget that toggles between 'six-inch' and 'footlong'.
+  // Switch widgets are toggled by tapping on them like buttons.
+  testWidgets('Switch toggles between six-inch and footlong',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const App());
+
+    // Find the Switch widget by key
+    final switchFinder = find.byKey(const Key('sandwichTypeSwitch'));
+
+    // Initially, OrderItemDisplay should show 'footlong'
+    expect(find.textContaining('footlong'), findsOneWidget);
+    expect(find.textContaining('six-inch'), findsNothing);
+
+    // Tap the Switch to toggle to 'six-inch'
+    await tester.tap(switchFinder);
+    await tester.pump();
+
+    // Now, OrderItemDisplay should show 'six-inch'
+    expect(find.textContaining('six-inch'), findsOneWidget);
+    expect(find.textContaining('footlong'), findsNothing);
+
+    // Tap again to toggle back to 'footlong'
+    await tester.tap(switchFinder);
+    await tester.pump();
+
+    // Should show 'footlong' again
+    expect(find.textContaining('footlong'), findsOneWidget);
+    expect(find.textContaining('six-inch'), findsNothing);
   });
 }
