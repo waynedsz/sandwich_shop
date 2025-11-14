1e775cfbee4067a39e8297ac220db4630fe90ed2
waynedsz <waynedesouza33@gmail.com>
2025-11-14 22:17:55 +0000

    fixed wrong worksheet

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:sandwich_shop/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0 by checking the OrderItemDisplay text.
     expect(find.textContaining('0 Footlong'), findsOneWidget);
