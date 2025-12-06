d29b718f565daa569c04c6598d0aaf6693f73842
waynedsz <waynedesouza33@gmail.com>
2025-12-06 20:50:02 +0000

    added missing StyledButton class

--- lib/views/order_screen.dart
+++ lib/views/order_screen.dart
@@ -309,3 +309,42 @@ class _OrderScreenState extends State<OrderScreen> {
     );
   }
 }
+
+class StyledButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+
+  final IconData icon;
+
+  final String label;
+
+  final Color backgroundColor;
+
+  const StyledButton({
+    super.key,
+    required this.onPressed,
+    required this.icon,
+    required this.label,
+    required this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ButtonStyle myButtonStyle = ElevatedButton.styleFrom(
+      backgroundColor: backgroundColor,
+      foregroundColor: Colors.white,
+      textStyle: normalText,
+    );
+
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: myButtonStyle,
+      child: Row(
+        children: [
+          Icon(icon),
+          const SizedBox(width: 8),
+          Text(label),
+        ],
+      ),
+    );
+  }
+}
