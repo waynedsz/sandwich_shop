a8f01d7963caf97dbe0a97b617e8b280d7456f88
waynedsz <waynedesouza33@gmail.com>
2025-12-10 13:15:15 +0000

    Add app_styles.dart with default text styles

---
+++ lib/views/app_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const TextStyle normalText = TextStyle(
+  fontSize: 16,
+);
+
+const heading1 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
