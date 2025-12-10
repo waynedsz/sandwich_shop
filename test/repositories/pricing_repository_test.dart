dc2beec3fec87d7f7374732e673369676996e52a
waynedsz <waynedesouza33@gmail.com>
2025-12-10 13:51:41 +0000

    Remove outdated tests and update PricingRepository test cases

--- test/repositories/pricing_repository_test.dart
+++ test/repositories/pricing_repository_test.dart
@@ -1,17 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-<<<<<<< HEAD
-import 'package:sandwich_shop/repositories/PricingRepository.dart';
-
-void main() {
-  test('Calculates price for six-inch sandwiches', () {
-    final repo = PricingRepository(quantity: 2, isFootlong: false);
-    expect(repo.getTotalPrice(), 14.0);
-  });
-
-  test('Calculates price for footlong sandwiches', () {
-    final repo = PricingRepository(quantity: 3, isFootlong: true);
-    expect(repo.getTotalPrice(), 33.0);
-=======
 import 'package:sandwich_shop/repositories/pricing_repository.dart';
 
 void main() {
@@ -32,6 +19,5 @@ void main() {
       final price = repository.calculatePrice(quantity: 0, isFootlong: true);
       expect(price, 0.00);
     });
->>>>>>> 92c6e97 (Add unit tests for pricing)
   });
 }
