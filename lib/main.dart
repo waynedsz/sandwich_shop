aa47bfb779eac640db4d5e0b898c8e66d409461e
waynedsz <waynedesouza33@gmail.com>
2025-12-10 12:55:15 +0000

    Define OrderScreen stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -49,3 +49,23 @@ class OrderItemDisplay extends StatelessWidget {
     return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
   }
 }
+
+class OrderScreen extends StatefulWidget {
+  final int maxQuantity;
+
+  const OrderScreen({super.key, this.maxQuantity = 10});
+
+  @override
+  State<OrderScreen> createState() {
+    return _OrderScreenState();
+  }
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  int _quantity = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
