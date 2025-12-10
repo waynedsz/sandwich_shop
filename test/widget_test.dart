703dbc8efaeed52dbb35f8942a94d12e77295615
waynedsz <waynedesouza33@gmail.com>
2025-12-10 15:56:26 +0000

    Fix import statement for OrderScreen in widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sandwich_shop/main.dart';
 import 'package:sandwich_shop/models/sandwich.dart';
 import 'package:sandwich_shop/views/common_widgets.dart';
+import 'package:sandwich_shop/views/order_screen.dart';
 
 void main() {
   group('App loads correctly', () {
