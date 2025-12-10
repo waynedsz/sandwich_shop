3a2b6a1b27d96adbf5b5c32ab1e41b0fba30b0e4
waynedsz <waynedesouza33@gmail.com>
2025-12-10 14:34:58 +0000

    Fix TextStyle declarations in app_styles.dart by removing merge conflict markers and ensuring consistent formatting.

--- lib/views/app_styles.dart
+++ lib/views/app_styles.dart
@@ -2,24 +2,14 @@ import 'package:flutter/material.dart';
 
 const TextStyle normalText = TextStyle(
   fontSize: 16,
-<<<<<<< HEAD
 );
 
-const heading1 = TextStyle(
+const TextStyle heading1 = TextStyle(
   fontSize: 24,
   fontWeight: FontWeight.bold,
 );
 
-const heading2 = TextStyle(
+const TextStyle heading2 = TextStyle(
   fontSize: 20,
   fontWeight: FontWeight.bold,
 );
-=======
-  fontWeight: FontWeight.bold,
-);
-
-const Heading1 = TextStyle(
-  fontSize: 24,
-  fontWeight: FontWeight.bold,
-);
->>>>>>> cdd80e0 (Move UIs to views)
